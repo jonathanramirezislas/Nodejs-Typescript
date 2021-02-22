32e1f6c4d73629d104c60158dc3b5cd848000b3a
Jonathan Ramirez <68352096+jonathanramirezislas@users.noreply.github.com>
2021-02-22 11:03:42 -0600

    middlewares

--- models/server.ts
+++ models/server.ts
@@ -1,5 +1,6 @@
 import express, { Application } from "express";
 import userRoutes from "../routes/usuarios";
+import cors from 'cors'
 
 class Server {
   private app: Application;
@@ -11,7 +12,22 @@ class Server {
   constructor() {
     this.app = express();
     this.port = process.env.PORT || "8000";
+
+    this.middleware();
     this.routes();
+  }
+
+  //TODO: base de datos
+
+  middleware(){
+  //cors
+    this.app.use(cors());
+  
+  //Read body from request
+  this.app.use(express.json());
+  //index for app
+  this.app.use(express.static('public'));
+
   }
 
   routes() {
