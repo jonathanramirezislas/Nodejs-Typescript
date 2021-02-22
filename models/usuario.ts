d05ec6268f35252c53dc0296f072949e3270fc87
Jonathan Ramirez <68352096+jonathanramirezislas@users.noreply.github.com>
2021-02-22 12:11:43 -0600

    model user

---
+++ models/usuario.ts
@@ -0,0 +1,16 @@
+import {DataType, DataTypes} from 'sequelize';
+import db from '../db/connection';
+
+const Usuario = db.define('Usuario',{
+    name:{
+        type: DataTypes.STRING
+    },
+    email:{
+        type: DataTypes.STRING
+    },
+    status:{
+        type: DataTypes.BOOLEAN
+    }
+});
+
+export default Usuario;
